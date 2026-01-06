f87795c74d4011c25935dbd486917126f9388941
xdai <daicongtu2910@gmail.com>
2026-01-06 15:54:58 +0700

    Part5

--- Demo_2d/Assets/Scips/Player/Bullet.cs
+++ Demo_2d/Assets/Scips/Player/Bullet.cs
@@ -5,6 +5,10 @@ public class Bullet : MonoBehaviour
     public float flySpeed = 10f;
     public int damage = 1;
 
+    private void Start()
+    {
+        GetComponent<AudioSource>().Play();
+    }
     void Update()
     {
         transform.position += Vector3.up * flySpeed * Time.deltaTime;
